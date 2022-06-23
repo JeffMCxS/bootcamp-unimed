21593cc2bd677c78a4c7de23260f3ce6a76021d8
Jeff <aurelio.sg3@gmail.com>
2022-06-23 20:25:27 -0300

    Introducao ao Angular - Fim da aula 2.3

--- ANGULAR/course-manager/src/app/app.module.ts
+++ ANGULAR/course-manager/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'; //Add
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component'; //Import do componente
 import { StarComponent } from './star/star.component'; //Add Import
+import { ReplacePipe } from './pipe/replace.pipe'; //Add Import
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent, //Informar (no modulo correspondente) para que possa ler o novo componente
-    StarComponent //Add novo componente
+    StarComponent, //Add novo componente
+    ReplacePipe //Add
   ],
   imports: [
     BrowserModule,
