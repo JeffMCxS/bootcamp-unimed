c60813d60684122fb8a5304de978ba7aae4f0336
Jeff <aurelio.sg3@gmail.com>
2022-07-24 14:24:58 -0300

    Primeiro commit - Desafio jogo RPG

---
+++ Desafios/dotNET/jogoRPG/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using jogoRPG.src.Entities;
+
+BlackWizard hero1 = new BlackWizard("Suaron");
+WhiteWizard hero2 = new WhiteWizard("Gandalf");
+Elf hero3 = new Elf("Legolas");
+Knight hero4 = new Knight("Aragorn", 1, 700, 20, 10);
+
+
+System.Console.WriteLine(hero1.Attack(10));
+System.Console.WriteLine("-------------------");
+System.Console.WriteLine(hero2.Attack(10));
+System.Console.WriteLine("-------------------");
+System.Console.WriteLine(hero3.Attack(10));
+System.Console.WriteLine("-------------------");
+System.Console.WriteLine(hero4.Attack(10));
+System.Console.WriteLine("-------------------");
+
+System.Console.WriteLine("");
+System.Console.WriteLine("Sequencia de ataques");
+System.Console.WriteLine("");
+
+System.Console.WriteLine(hero4.Attack(10));
+System.Console.WriteLine("");
+System.Console.WriteLine(hero4.Attack(10));
+System.Console.WriteLine(hero4.Attack(10));
+System.Console.WriteLine(hero4.Attack(10));
+
+
+
